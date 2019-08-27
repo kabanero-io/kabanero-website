23f889a40b93b15da65b89462249dfbc79350d71
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-08-27 13:47:24 -0400

    last changes

--- src/main/content/_assets/js/guide-markdown.js
+++ src/main/content/_assets/js/guide-markdown.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
     let guideContentSection = $('#guide-content-section');
 
-        $('div#guide-content-section h2').each(function(i, header) {
-            let sect1 = $("<div/>", {class: "sect1"});
-            let sectionbody = $("<div/>", {class: "sectionbody"});
-            let paragraph = $("<div/>", {paragraph: "sectionbody"});
+    $('div#guide-content-section h2').each(function (i, header) {
+        let sect1 = $("<div/>", { class: "sect1" });
+        let sectionbody = $("<div/>", { class: "sectionbody" });
+        let paragraph = $("<div/>", { class: "paragraph" });
 
-            $(`#${header.id}`).nextUntil($(`h2`)).each(function (index, element) {
-                sect1.append(header)
-                if (element.className !== 'sect1') {
-                    paragraph.append(element);
-                }
-                if (element.nodeName === 'H3') {
-                    let sect2 = $("<div/>", {class: "sect2"});
-                    sect2.append(element)
-                    paragraph.append(sect2);
-                }
-            });
-            sectionbody.append(paragraph);
-            sect1.append(sectionbody);
-            guideContentSection.append(sect1);
+        $(`#${header.id}`).nextUntil($(`h2`)).each(function (index, element) {
+            sect1.append(header)
+            if (element.className !== 'sect1') {
+                paragraph.append(element);
+            }
+            if (element.nodeName === 'H3') {
+                let sect2 = $("<div/>", { class: "sect2" });
+                sect2.append(element)
+                paragraph.append(sect2);
+            }
         });
+        sectionbody.append(paragraph);
+        sect1.append(sectionbody);
+        guideContentSection.append(sect1);
+    });
 
 
     $('#toc-container').append($('#markdown-toc'))
     if ($('#markdown-toc > li > ul')) {
         $('#markdown-toc > li > ul').addClass('sectlevel2')
     }
-    
+
     var li = $("<li/>");
     var a = $("<a/>");
     var end_of_guide_title = $('#end_of_guide > h2').first().text();

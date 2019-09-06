8077004302295185cad6909ee12c81cd86e5fd0e
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-06 13:03:24 -0400

    bla

--- scripts/ibmcloud-cf-push.sh
+++ scripts/ibmcloud-cf-push.sh
@@ -41,5 +41,5 @@ else
     ibmcloud cf install-plugin -f blue-green-deploy -r CF-Community
 fi
 
-
+echo ibmcloud cf blue-green-deploy $APP_NAME --delete-old-apps --smoke-test "$CUR_DIR"/smoke-test.py
 ibmcloud cf blue-green-deploy $APP_NAME --delete-old-apps --smoke-test "$CUR_DIR"/smoke-test.py

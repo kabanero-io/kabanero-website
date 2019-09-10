99a9ffdc8eff395355ef7705a3221fcf3c139f1e
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-10 16:18:15 -0400

    last

--- scripts/build_clone_docs.sh
+++ scripts/build_clone_docs.sh
@@ -18,6 +18,10 @@ cd docs
 
 # This is how you clone a repo without autocreating a parent folder with the name of the repo
 # The clone is picky about cloning into a folder that is not empty (src/main/content)
-git clone git@github.com:kabanero-io/docs.git --branch $branch_name .
+if [ -n "$1" ] && [ -n "$2" ]; then
+	git clone "$1" --branch "$2" .
+else
+    git clone https://github.com/kabanero-io/blogs --branch master .
+fi
 popd
 echo "Done cloning docs repository!"

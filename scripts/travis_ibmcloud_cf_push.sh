d4e72cf20331ffee856c544ee47cb7205aed2ac6
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 14:06:09 -0400

    final push

--- scripts/travis_ibmcloud_cf_push.sh
+++ scripts/travis_ibmcloud_cf_push.sh
@@ -7,8 +7,8 @@ source $SCRIPT_DIR/build_clone_docs.sh    $DOCS_GIT_URL   $DOCS_GIT_REVISION
 source $SCRIPT_DIR/build.sh
 
 if [ "$JEKYLL_ENV" == "production" ]; then
-    source $SCRIPT_DIR/bmcloud-cf-push.sh "kabanero-prod"
+    source $SCRIPT_DIR/ibmcloud-cf-push.sh "kabanero-prod"
 else
 
-    source $SCRIPT_DIR/bmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}-kabanero"
+    source $SCRIPT_DIR/ibmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}-kabanero"
 fi

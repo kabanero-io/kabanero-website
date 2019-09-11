5b87a30e25a87939be399e62ce189e6a7a2011d1
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 13:45:45 -0400

    setting env var

--- scripts/travis_ibmcloud_cf_push.sh
+++ scripts/travis_ibmcloud_cf_push.sh
@@ -5,8 +5,8 @@ curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 source $SCRIPT_DIR/build_clone_blogs.sh   $BLOGS_GIT_URL  $BLOGS_GIT_REVISION
 source $SCRIPT_DIR/build_clone_docs.sh  $DOCS_GIT_URL   $DOCS_GIT_REVISION
 source $SCRIPT_DIR/build.sh
-
-if [ "$JEKYLL_ENV"="production" ]; then
+echo "$JEKYLL_ENV"
+if [ "$JEKYLL_ENV"=="production" ]; then
     echo "production build"
     #source $SCRIPT_DIR/bmcloud-cf-push.sh kabanero-prod
 else

b6152df46429f5aa34b1e5e3107ef82323d7089f
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 13:17:23 -0400

    updating env var

--- scripts/travis_ibmcloud_cf_push.sh
+++ scripts/travis_ibmcloud_cf_push.sh
@@ -2,15 +2,20 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
-source $SCRIPT_DIR/build_clone_blogs.sh "$BLOGS_GIT_URL" "$BLOGS_GIT_REVISION"
-source $SCRIPT_DIR/build_clone_docs.sh  "$DOCS_GIT_URL"  "$DOCS_GIT_REVISION"
-source $SCRIPT_DIR/build.sh
+source curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+source "$SCRIPT_DIR"/build_clone_blogs.sh "$BLOGS_GIT_URL" "$BLOGS_GIT_REVISION"
+source "$SCRIPT_DIR"/build_clone_docs.sh  "$DOCS_GIT_URL"  "$DOCS_GIT_REVISION"
+source "$SCRIPT_DIR"/build.sh
 
-if [ $JEKYLL_ENV="production" ]; then
+if [ "$JEKYLL_ENV"="production" ]; then
     echo "production build"
     #source $SCRIPT_DIR/bmcloud-cf-push.sh kabanero-prod
 else
     echo "dev build"
+    echo $SCRIPT_DIR/build_clone_blogs.sh $BLOGS_GIT_URL $BLOGS_GIT_REVISION
+    echo $SCRIPT_DIR/build_clone_docs.sh  "$DOCS_GIT_URL"  "$DOCS_GIT_REVISION"
+    echo "$SCRIPT_DIR"/build.sh
+    echo "$JEKYLL_ENV"
+    echo "${TRAVIS_REPO_SLUG%/*}"
     #source $SCRIPT_DIR/bmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}"-kabanero
 fi

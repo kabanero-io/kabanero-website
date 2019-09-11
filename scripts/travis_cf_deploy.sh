5244ce16f7f6fa45a84adeec077c272bf87f8d16
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 12:49:22 -0400

    requested changes

---
+++ scripts/travis_cf_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname $0)
+
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+source $SCRIPT_DIR/build_clone_blogs.sh "$BLOGS_GIT_URL" "$BLOGS_GIT_REVISION"
+source $SCRIPT_DIR/build_clone_docs.sh  "$DOCS_GIT_URL"  "$DOCS_GIT_REVISION"
+source $SCRIPT_DIR/build.sh
+
+if [ $JEKYLL_ENV="production" ]; then
+    source $SCRIPT_DIR/bmcloud-cf-push.sh kabanero-prod
+else
+    source $SCRIPT_DIR/bmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}"-kabanero
+fi

69591cf1133ee24bd3bb6008242ed77bd113a3d8
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 17:22:14 -0400

    upading version

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 #
 apt-get update
 apt-get install libgdbm-dev libncurses5-dev automake libtool bison libffi-dev -y
-sudo apt-get install python3.6 -y
+apt-get install python3.6 -y
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 source /home/pipeline/.rvm/scripts/rvm

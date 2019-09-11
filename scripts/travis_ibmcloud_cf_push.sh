10a94d3841f5c88374e84f08a0e45c4b4e110c86
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-11 13:59:20 -0400

    running script

--- scripts/travis_ibmcloud_cf_push.sh
+++ scripts/travis_ibmcloud_cf_push.sh
@@ -3,15 +3,12 @@ SCRIPT_DIR=$(dirname $0)
 
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 source $SCRIPT_DIR/build_clone_blogs.sh   $BLOGS_GIT_URL  $BLOGS_GIT_REVISION
-source $SCRIPT_DIR/build_clone_docs.sh  $DOCS_GIT_URL   $DOCS_GIT_REVISION
+source $SCRIPT_DIR/build_clone_docs.sh    $DOCS_GIT_URL   $DOCS_GIT_REVISION
 source $SCRIPT_DIR/build.sh
-echo "$JEKYLL_ENV"
+
 if [ "$JEKYLL_ENV" == "production" ]; then
-    echo "production build"
-    #source $SCRIPT_DIR/bmcloud-cf-push.sh kabanero-prod
+    source $SCRIPT_DIR/bmcloud-cf-push.sh kabanero-prod
 else
-    echo "dev build"
-    echo "$JEKYLL_ENV"
-    echo "${TRAVIS_REPO_SLUG%/*}"
-    #source $SCRIPT_DIR/bmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}"-kabanero
+
+    source $SCRIPT_DIR/bmcloud-cf-push.sh "${TRAVIS_REPO_SLUG%/*}"-kabanero
 fi

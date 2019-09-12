28c69ffdf93fefd3f28e09815cfc1c1dbcb6c725
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-12 14:38:09 -0400

    updates

--- scripts/travis_ibmcloud_cf_push.sh
+++ scripts/travis_ibmcloud_cf_push.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname $0)
 curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 $SCRIPT_DIR/build_clone_blogs.sh   $BLOGS_GIT_URL  $BLOGS_GIT_REVISION
 $SCRIPT_DIR/build_clone_docs.sh    $DOCS_GIT_URL   $DOCS_GIT_REVISION
-$SCRIPT_DIR/build.sh
+$SCRIPT_DIR/build_jekyll_maven.sh
 
 if [ "$JEKYLL_ENV" == "production" ]; then
     $SCRIPT_DIR/ibmcloud-cf-push.sh kabanero-prod

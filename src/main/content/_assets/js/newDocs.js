fafe2ef7e9487152d7d1f8049663791b378b98be
Kevin See <kevinsee@kevins-mbp-2.raleigh.ibm.com>
2019-09-17 10:16:58 -0400

    more ui for new docs

--- src/main/content/_assets/js/newDocs.js
+++ src/main/content/_assets/js/newDocs.js
@@ -4,10 +4,23 @@ function loadContent(href){
 
 $(".toc-category").click(function(){
     $(this).parent().toggleClass('open');
+    console.log($(this).parent().find(".plus-minus-icon").attr('src') === '/img/icon_plus.png');
+    $(this).parent().find(".plus-minus-icon").attr('src') === '/img/icon_plus.png' ? $(this).parent().find(".plus-minus-icon").attr('src', '/img/icon_minus.png') : $(this).parent().find(".plus-minus-icon").attr('src', '/img/icon_plus.png');
 });
 
 $(".doc-title").click(function(){
     loadContent($(this).attr('href'));
     $('.activeDoc').removeClass('activeDoc');
     $(this).addClass('activeDoc');
-})
+});
+
+function selectDocInToc(){
+    let currentHref = window.location.href;
+    let selectedFile = '/docs/ref/general' + currentHref.substring(currentHref.lastIndexOf('/'));
+    $(`a[href$="${selectedFile}"]`).addClass('active-doc')
+    $( `a[href$="${selectedFile}"]` ).parent().parent().parent().find('.toc-category').click();
+}
+
+$(document).ready(function(){
+    selectDocInToc();
+});

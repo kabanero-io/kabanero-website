ffc572e44758a1933b7d9b4561505f7bdf92da40
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-17 13:31:34 -0400

    updating docker image name used for travis builds

--- travis/docker_run.sh
+++ travis/docker_run.sh
@@ -11,4 +11,4 @@ docker run -it \
           -e BLOGS_GIT_URL=${BLOGS_GIT_URL} \
           -e JEKYLL_ENV=${JEKYLL_ENV} \
           -e TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG%/*} \
-          travis-testing bash -c "$1" 
+          travis bash -c "$1" 

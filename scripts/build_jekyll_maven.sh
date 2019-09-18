0432a386d163c03ce29e2a45c29e02635920fd88
Andrew Lohr <ajlohr@us.ibm.com>
2019-09-18 17:32:19 -0400

    clone draft guides when needed

--- scripts/build_jekyll_maven.sh
+++ scripts/build_jekyll_maven.sh
@@ -1,48 +1,37 @@
-#  This script contains the end-to-end steps for building the website with Jekyll and using Maven to package
-#
-#
-
+#!/bin/bash
 # Exit immediately if a simple command exits with a non-zero status.
 set -e
-
 JEKYLL_BUILD_FLAGS=""
+CONTENT_DIR="src/main/content"
+TARGET_DIR="target"
+
+echo "Ruby version:"
+ruby -v
 
 ./scripts/build_gem_dependencies.sh
 
-echo "Ruby version:"
-echo `ruby -v`
-
+# Guides that are ready to be published to the Code Conjuring site
 echo "Cloning repositories with name starting with guide or iguide..."
 ruby ./scripts/build_clone_guides.rb
 
 # Development environment only actions
 if [ "$JEKYLL_ENV" != "production" ]; then 
-    echo "Not in production environment"
-    # Guides that are ready to be published to the Code Conjuring site
+    echo "Not in production environment..."
     echo "Adding robots.txt"
-    cp robots.txt src/main/content/robots.txt
+    cp robots.txt "$CONTENT_DIR"/robots.txt
 
     # Development environments with draft docs/guides
     if [ "$JEKYLL_DRAFT_GUIDES" == "true" ]; then
         echo "Clone draft guides for test environments..."
-        #ruby ./scripts/build_clone_guides.rb "draft-guide"    
+        ruby ./scripts/build_clone_guides.rb "draft-guide"    
 
-        # Need to make sure there are draft-iguide* folders before using the find command
-        # If we don't, the find command will fail because the path does not exist
-        if [ $(find src/main/content/guides -type d -name "draft-iguide*" | wc -l ) != "0" ] ; then
-            echo "Moving any js and css files from draft interactive guides..."
-            #find src/main/content/guides/draft-iguide* -d -name js -exec cp -R '{}' src/main/content/_assets \;
-            #find src/main/content/guides/draft-iguide* -d -name css -exec cp -R '{}' src/main/content/_assets \;
-        fi
-        ./scripts/build_clone_docs.sh "draft" # Argument is branch name of ICP4APPs/docs
+        #./scripts/build_clone_docs.sh "draft" # Argument is branch name of kabanero-io/docs
     else
-        ./scripts/build_clone_docs.sh "master" # Argument is branch name of ICP4APPs/docs
+        echo "not cloning draft guides"
     fi
-else
-    # Production!
-    echo "Clone published docs!"
-    ./scripts/build_clone_docs.sh "master" # Argument is branch name of ICP4APPs/docs
 fi
+
+./scripts/build_clone_docs.sh "master" # Argument is branch name of kabanero-io/docs
 
 # Development environments that enable the draft blogs in the _draft directory.
 if [ "$JEKYLL_DRAFT_BLOGS" == "true" ]; then
@@ -50,25 +39,14 @@ if [ "$JEKYLL_DRAFT_BLOGS" == "true" ]; then
     JEKYLL_BUILD_FLAGS="--drafts"
 fi
 
-# Special handling for javadocs
-#./scripts/modify_javadoc.sh
-
 echo "Copying guide images to /img/guide"
-mkdir -p src/main/content/img/guide
-# Check if any draft guide images exist first
-if [ -e src/main/content/guides/draft-guide*/assets/* ]
- then cp src/main/content/guides/draft-guide*/assets/* src/main/content/img/guide/
-fi
+mkdir -p "$CONTENT_DIR"/img/guide
 # Check if any published guide images exist first
-if [ -e src/main/content/guides/guide*/assets/* ]
- then cp src/main/content/guides/guide*/assets/* src/main/content/img/guide/
-fi
-
-# Move any js/css files from guides to the _assets folder for jekyll-assets minification.
-echo "Moving any js and css files published interactive guides..."
-# Assumption: There is _always_ iguide* folders
-#find src/main/content/guides/iguide* -d -name js -exec cp -R '{}' src/main/content/_assets \;
-#find src/main/content/guides/iguide* -d -name css -exec cp -R '{}' src/main/content/_assets \;
+for GUIDE in $( ls "$CONTENT_DIR"/guides ); do
+	if [[ -d "$CONTENT_DIR/guides/$GUIDE"/assets ]]; then
+		cp "$CONTENT_DIR/guides/$GUIDE"/assets/* "$CONTENT_DIR"/img/guide/
+	fi
+done
 
 # Build draft and published blogs
 ./scripts/build_clone_blogs.sh
@@ -76,9 +54,8 @@ echo "Moving any js and css files published interactive guides..."
 # Jekyll build
 echo "Building with jekyll..."
 echo `jekyll -version`
-mkdir -p target/jekyll-webapp
-jekyll build $JEKYLL_BUILD_FLAGS --source src/main/content --destination target/jekyll-webapp
-#python3 ./scripts/parse-feature-toc.py
+mkdir -p "$TARGET_DIR"/jekyll-webapp
+bundle exec jekyll build $JEKYLL_BUILD_FLAGS --source "$CONTENT_DIR" --destination "$TARGET_DIR"/jekyll-webapp
 
 # Maven packaging
 echo "Running maven (mvn)..."

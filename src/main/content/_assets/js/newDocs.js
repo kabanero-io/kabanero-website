5a2f68f6bdce02ca2c98c9c66442ac8410fc49f0
Kevin See <kevinsee@kevins-mbp-2.raleigh.ibm.com>
2019-09-18 11:25:41 -0400

    implemented search

--- src/main/content/_assets/js/newDocs.js
+++ src/main/content/_assets/js/newDocs.js
@@ -4,7 +4,6 @@ function loadContent(href){
 
 $(".toc-category").click(function(){
     $(this).parent().toggleClass('open');
-    console.log($(this).parent().find(".plus-minus-icon").attr('src') === '/img/icon_plus.png');
     $(this).parent().find(".plus-minus-icon").attr('src') === '/img/icon_plus.png' ? $(this).parent().find(".plus-minus-icon").attr('src', '/img/icon_minus.png') : $(this).parent().find(".plus-minus-icon").attr('src', '/img/icon_plus.png');
 });
 
@@ -14,11 +13,54 @@ $(".doc-title").click(function(){
     $(this).addClass('activeDoc');
 });
 
+$('#doc-search').keyup(function(){
+    searchDocs();
+});
+
+function searchDocs(){
+    let searchTerm = document.getElementById('doc-search').value.toLowerCase();
+    let docTitles = $('.doc-title');
+    $.each(docTitles, function(index, value){
+        if($(value).attr('id') !== 'welcome-doc'){
+            let docTitle = $(value).text().toLowerCase();
+            console.log(docTitle);
+            console.log(!$(value).hasClass('active-doc'));
+            !docTitle.includes(searchTerm) && !$(value).hasClass('active-doc') ? $(value).hide() : $(value).show();
+            console.log(!docTitle.includes(searchTerm) && !$(value).hasClass('active-doc'));
+        }
+    });
+    hideCategoriesIfEmpty();
+    $('.toc-item:visible').length === 0 ? $('#noSearchResults').removeClass('no-display') : $('#noSearchResults').addClass('no-display');
+}
+
+function hideCategoriesIfEmpty(){
+    let categories = $('.toc-item');
+    $.each(categories, function(index, category){
+        if($(category).attr('id') !== 'welcome-doc'){
+            let childLinks = $(category).find('.doc-title');
+            let isVisible = areLinksVisible(childLinks);
+            isVisible ? $(category).show() : $(category).hide();
+        }
+    });
+}
+
+function areLinksVisible(links){
+    let areVisible = false;
+    $.each(links, function(index, link){
+        if($(link).css('display') !== 'none'){
+            areVisible = true;
+        }
+    });
+    return areVisible;
+}
+
 function selectDocInToc(){
     let currentHref = window.location.href;
     let selectedFile = '/docs/ref/general' + currentHref.substring(currentHref.lastIndexOf('/'));
-    $(`a[href$="${selectedFile}"]`).addClass('active-doc')
-    $( `a[href$="${selectedFile}"]` ).parent().parent().parent().find('.toc-category').click();
+    if(selectedFile !== '/docs/ref/general/docs-welcome.html'){
+        $(`a[href$="${selectedFile}"]`).addClass('active-doc')
+        $( `a[href$="${selectedFile}"]` ).parent().parent().parent().find('.toc-category').click();
+    }
 }
 
 $(document).ready(function(){

e3fa841ff6c7276d4a4d0e6f434b01edf3fd5843
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-23 14:06:08 -0400

    test

--- travis/repo.sh
+++ travis/repo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ "$TRAVIS_REPO_SLUG" != "kidus60/kabanero-website" ];then
-        echo "Only builds from kubernetes/ingress-nginx repository is allowed.";  
+        echo "Only builds from kabanero-io/kabanero-website repository is allowed.";  
         exit 0;
     fi

f89774a7aafa68516d199bfc19ecce01e116a429
Andrew Lohr <alohr51@users.noreply.github.com>
2019-09-24 15:01:19 -0400

    float toc (#228)

--- src/main/content/_assets/js/toc.js
+++ src/main/content/_assets/js/toc.js
@@ -41,7 +41,12 @@ function disableFloatingTOC() {
 }
 
 function enableFloatingTOC() {
-    $('#toc-inner').css({'position':'fixed', 'top':'100px'});    
+    // The toc has a fixed position- this will allow it to scroll with the user and stay "snapped" at the top once the header disappears
+    var heightOfHeaderPix = 100;
+    var pixFromTop = $(window).scrollTop();
+    var newTop = heightOfHeaderPix - pixFromTop;
+    var tocTop = newTop < 0 ? 0 : newTop;
+    $('#toc-inner').css({'position':'fixed', 'top': tocTop});    
 }
 
 function calculateTOCHeight(){

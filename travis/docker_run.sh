4f58a273d4068f6f4c4a11495cd067b40cdbc474
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-24 17:08:13 -0400

    fixing format

--- travis/docker_run.sh
+++ travis/docker_run.sh
@@ -11,4 +11,5 @@ docker run -it \
           -e BLOGS_GIT_URL=${BLOGS_GIT_URL} \
           -e JEKYLL_ENV=${JEKYLL_ENV} \
           -e TRAVIS_REPO_SLUG=${TRAVIS_REPO_SLUG%/*} \
+          -e TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST} \
           website-travis bash -c "$1" 

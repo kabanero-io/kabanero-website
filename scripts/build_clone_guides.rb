ece5dc16ec0f48cb8a742287b9a2b0704a7e5fca
kidus.sw@ibm.com <kidus.sw@ibm.com@10.0.0.155>
2019-09-27 12:33:25 -0400

    addign gh token to travis

--- scripts/build_clone_guides.rb
+++ scripts/build_clone_guides.rb
@@ -19,7 +19,7 @@ cloneDraftGuides = ARGV[0]
 client = Octokit::Client.new :access_token => ENV['PAT']
 client.auto_paginate = true
 repos = client.org_repos('kabanero-io')
-
+print client.ratelimit
 # --------------------------------------------
 # Travis CI related steps
 # --------------------------------------------

7224d3211de552c903025f14152e7eadeded3e20
Andrew Lohr <alohr51@users.noreply.github.com>
2019-09-30 13:15:05 -0400

    make sure to only clone specific revision of docs (#239)

--- scripts/build_clone_docs.sh
+++ scripts/build_clone_docs.sh
@@ -4,13 +4,14 @@
 # this sequence of commands to workaround that limitation. 
 # Could _not_ use:
 #   git clone git@github.com:kabanero-io/docs.git --branch develop src/main/content
+DOCS_GIT_REVISION=${1:-master}
 
 CUR_DIR="$(cd $(dirname $0) && pwd)"
 pushd "$CUR_DIR/../src/main/content"
 # Remove the folder to allow this repeating execution of this script
 rm -rf docs
 
-echo "Start cloning docs repository..."
+echo "Start cloning docs repository with revision: ${DOCS_GIT_REVISION}"
 mkdir docs
 cd docs
 
@@ -19,7 +20,7 @@ cd docs
 if [ -n "$1" ] && [ -n "$2" ]; then
 	git clone "$1" --branch "$2" .
 else
-    git clone https://github.com/kabanero-io/docs --branch master .
+    git clone https://github.com/kabanero-io/docs --branch ${DOCS_GIT_REVISION} .
 fi
 popd
 echo "Done cloning docs repository!"

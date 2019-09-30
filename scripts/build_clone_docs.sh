dc9b50aaed7a0ca468b2326c7d70bf9b5c5d91b5
Andrew Lohr <ajlohr@us.ibm.com>
2019-09-30 13:56:06 -0400

    docs repo as parameter on clone

--- scripts/build_clone_docs.sh
+++ scripts/build_clone_docs.sh
@@ -4,23 +4,22 @@
 # this sequence of commands to workaround that limitation. 
 # Could _not_ use:
 #   git clone git@github.com:kabanero-io/docs.git --branch develop src/main/content
-DOCS_GIT_REVISION=${1:-master}
+DOCS_GIT_URL=${1:-"https://github.com/kabanero-io/docs"}
+DOCS_GIT_REVISION=${2:-"master"}
 
 CUR_DIR="$(cd $(dirname $0) && pwd)"
 pushd "$CUR_DIR/../src/main/content"
+
 # Remove the folder to allow this repeating execution of this script
 rm -rf docs
 
-echo "Start cloning docs repository with revision: ${DOCS_GIT_REVISION}"
+echo "Start cloning docs repository: ${DOCS_GIT_URL} with revision: ${DOCS_GIT_REVISION}"
 mkdir docs
 cd docs
 
 # This is how you clone a repo without autocreating a parent folder with the name of the repo
 # The clone is picky about cloning into a folder that is not empty (src/main/content)
-if [ -n "$1" ] && [ -n "$2" ]; then
-	git clone "$1" --branch "$2" .
-else
-    git clone https://github.com/kabanero-io/docs --branch ${DOCS_GIT_REVISION} .
-fi
+git clone "${DOCS_GIT_URL}" --branch "${DOCS_GIT_REVISION}" .
+
 popd
-echo "Done cloning docs repository!"
+echo "Done cloning docs repository"

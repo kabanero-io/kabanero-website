8a89a4161c2c7b78c4a7ff3b6b9355bd5a9787ce
kidus60 <39460219+kidus60@users.noreply.github.com>
2019-10-17 15:40:45 -0400

    Updating docs to show all versions  (#246)
    * updating doc render
    
    * adding latest to doc dripdown
    
    * update
    
    * final changes
    
    * final touches
    
    * updating translation
    
    * Update docs.scss
    
    * fixing indenting
    
    * quick fix for toc indicator
    
    * updating doc version pr
    
    * fixing indent
    
    * chang
    
    * revergting
    
    * removing redundant liquid tags
    
    * updating script to mock produciton build
    
    * script update
    
    * adding semicolon to js file
    
    * updating script
    
    * final touches
    
    * final touches
    
    * script updatate
    
    * updating latest tag command
    
    * updating script
    
    * indenting liquid tags
    
    * adding back whats new page layout

--- scripts/build_clone_docs.sh
+++ scripts/build_clone_docs.sh
@@ -1,25 +1,57 @@
 # Clone the docs (draft or published)
 #
 # git clone does not like to clone into folders that are populated.  We are doing
-# this sequence of commands to workaround that limitation. 
+# this sequence of commands to workaround that limitation.
 # Could _not_ use:
 #   git clone git@github.com:kabanero-io/docs.git --branch develop src/main/content
 DOCS_GIT_URL=${1:-"https://github.com/kabanero-io/docs"}
 DOCS_GIT_REVISION=${2:-"master"}
 
 CUR_DIR="$(cd $(dirname $0) && pwd)"
+
+git_clone_doc_tags() {
+
+    #get all tags from the repo
+    TAGS=$(git tag -l --sort=-v:refname)
+    #get the latest tag from repo
+    LATEST_TAG=$(git tag -l --sort=v:refname | tail -1)    
+
+    git checkout $LATEST_TAG
+    echo -e -n "{\"latest\":\""$LATEST_TAG"\", \"versions\":["  >> docversions.json
+
+    for TAG in $TAGS; do
+    echo -e -n \"$TAG\", >> temp.json
+
+        # for all tags other than the latest we clone them into a folder of thier own with the folder name being the version of doc under the /docs dir
+        if [ "$TAG" != "$LATEST_TAG" ]; then
+            mkdir $TAG && git --work-tree=$TAG checkout $TAG -- .
+        fi
+    done
+
+    sed '$ s/,$//g' temp.json >> docversions.json 
+    rm -f temp.json
+
+    echo -e -n "]}" >> docversions.json
+}
 pushd "$CUR_DIR/../src/main/content"
 
 # Remove the folder to allow this repeating execution of this script
 rm -rf docs
 
 echo "Start cloning docs repository: ${DOCS_GIT_URL} with revision: ${DOCS_GIT_REVISION}"
-mkdir docs
-cd docs
+mkdir docs && cd docs
 
 # This is how you clone a repo without autocreating a parent folder with the name of the repo
 # The clone is picky about cloning into a folder that is not empty (src/main/content)
+
+#If we are not in production we only cloned the specified github url and branch and give it the version number 0.0.0
 git clone "${DOCS_GIT_URL}" --branch "${DOCS_GIT_REVISION}" .
+
+if [ "$JEKYLL_ENV" == "production" ]; then
+    git_clone_doc_tags
+fi
 
 popd
 echo "Done cloning docs repository"
+
+
